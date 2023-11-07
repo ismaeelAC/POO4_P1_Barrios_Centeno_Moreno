6797643e75d17623c57198b544c5448bbd46c255
ismaeelAC <149702013+ismaeelAC@users.noreply.github.com>
2023-11-07 14:50:57 -0500

    Primer commit

---
+++ src/poo4_p1_barrios_centeno_moreno/POO4_P1_Barrios_Centeno_Moreno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo4_p1_barrios_centeno_moreno;
+
+/**
+ *
+ * @author LNV
+ */
+public class POO4_P1_Barrios_Centeno_Moreno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
