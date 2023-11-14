a4606383a31395f808da57e530eee1c39b7e643b
Roberto C Barrios. <robauret@espol.edu.ec>
2023-11-13 21:27:00 -0500

    Creando clases del proyecto

---
+++ src/poo4_p1_barrios_centeno_moreno/Estado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package poo4_p1_barrios_centeno_moreno;
+
+/**
+ *
+ * @author rb122
+ */
+public enum Estado {
+    O,D
+}
