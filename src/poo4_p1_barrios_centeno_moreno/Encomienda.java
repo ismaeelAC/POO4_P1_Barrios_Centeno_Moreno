a8f6dfdeafbddbd9a23432ca45e507b116ba5568
ismaeelAC <iacenteen@espol.edu.ec>
2023-11-13 21:32:50 -0500

    Creando clase servicio y su herencia

---
+++ src/poo4_p1_barrios_centeno_moreno/Encomienda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo4_p1_barrios_centeno_moreno;
+
+/**
+ *
+ * @author LNV
+ */
+public class Encomienda extends Servicio {
+    private double peso;
+    //private TipoEncomienda tipoE;
+    private int numProductos;
+}
