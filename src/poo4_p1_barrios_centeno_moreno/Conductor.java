70c8292775c2afee910ce3daa02b964f88afb1ed
Stephany Moreno <steemore@espol.edu.ec>
2023-11-13 21:28:50 -0500

    Clase Usuario y herencia

---
+++ src/poo4_p1_barrios_centeno_moreno/Conductor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo4_p1_barrios_centeno_moreno;
+
+/**
+ *
+ * @author grils
+ */
+public class Conductor extends Usuario{
+    private String licencia;
+    private Estado estado;
+    private Vehiculo vehiculo;
+    
+}
---
+++ src/poo4_p1_barrios_centeno_moreno/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo4_p1_barrios_centeno_moreno;
+
+/**
+ *
+ * @author grils
+ */
+public class Usuario {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String user;
+    private String contrasenia;
+    private String celular;
+    private TipoUsuario tipoUsuario;
+
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    
+    public String getContrasenia(){
+        return contrasenia;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+    
+    
+    
+    
+}
+
