8b754e7194c907fae653b8efde4c7e59362798cd
Roberto C Barrios. <robauret@espol.edu.ec>
2024-01-21 15:41:17 -0500

    Creacion Ventana de Reserva y Ventana de vuelos disponibles

---
+++ Proyecto2P/src/main/java/com/mycompany/par4_proy2p_barrios_centeno_moreno/FXMLController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.par4_proy2p_barrios_centeno_moreno;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author rb122
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
